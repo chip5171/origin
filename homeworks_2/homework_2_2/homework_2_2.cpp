6ab4652164164b9812847f3089088a0c578ec609
Natalya Lyukshina <chip5171@gmail.com>
2022-09-28 13:01:35 +0300

    added files homework_2_2

---
+++ homeworks_2/homework_2_2/homework_2_2.cpp
@@ -0,0 +1,40 @@
+﻿// Задача 2. Счета
+
+#include <iostream>
+#include <Windows.h>
+
+struct BankAccount {
+    int accountNumber;
+    std::string firstName;
+    double balance;
+};
+
+void changeBalance(BankAccount& b, double newBalance) {
+    b.balance = newBalance;
+}
+
+int main(int argc, char** argv) {
+    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    BankAccount b; 
+    double newBalance;
+    std::cout << "Введите номер счета: ";
+    std::cin >> b.accountNumber;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> b.firstName;
+    std::cout << "Введите баланс: ";
+    std::cin >> b.balance;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newBalance;
+
+    changeBalance(b, newBalance);
+
+    std::cout << "Ваш счет: " << b.firstName << ", " << b.accountNumber << ", " << b.balance << std::endl;
+    
+    return 0;
+}
+
+
+
