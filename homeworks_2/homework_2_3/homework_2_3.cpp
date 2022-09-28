54061f48b9246a1301db44b3312fd5f601b14ab1
Natalya Lyukshina <chip5171@gmail.com>
2022-09-28 16:20:21 +0300

    changed files homework_2_3

--- homeworks_2/homework_2_3/homework_2_3.cpp
+++ homeworks_2/homework_2_3/homework_2_3.cpp
@@ -24,23 +24,10 @@ int main(int argc, char** argv) {
     setlocale(LC_ALL, "rus");
 
     Address a1, a2, a3;
-    a1.sity = "Москва";
-    a1.street = "Арбат";
-    a1.house = 12;
-    a1.apartment = 8;
-    a1.index = 123456;
 
-    a2.sity = "Ижевск";
-    a2.street = "Пушкина";
-    a2.house = 59;
-    a2.apartment = 143;
-    a2.index = 953769;
-
-    a3.sity = "Тверь";
-    a3.street = "Ленина";
-    a3.house = 20;
-    a3.apartment = 15;
-    a3.index = 654321;
+    a1 = { "Москва", "Арбат", 12, 8, 123456 };
+    a2 = { "Ижевск", "Пушкина", 59, 143, 953769 };
+    a3 = { "Тверь", "Ленина", 20, 15, 654321 };
 
     structurePrinting(a1);
     structurePrinting(a2);
