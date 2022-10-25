86667cfff2e133267a9cf6de6c4d6e91ae125c4b
Natalia Lyukshina <chip5171@gmail.com>
2022-10-25 15:12:29 +0900

    modified homework_7_1

--- homeworks_7/homework_7_1/homework_7_1.cpp
+++ homeworks_7/homework_7_1/homework_7_1.cpp
@@ -3,13 +3,12 @@
 #include <iostream>
 #define MODE 1
 
-int func(int x, int y) {
+int add(int x, int y) {
     return (x + y);
 }
 
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "rus");
-    int x, y;
 
 #ifndef MODE
 #error Сonstant is not defined
@@ -17,12 +16,13 @@ int main(int argc, char** argv) {
 #if MODE == 0 
     std::cout << "Работаю в режиме тренировки" << std::endl;
 #elif MODE == 1
+    int x, y;
     std::cout << "Работаю в боевом режиме" << std::endl;
     std::cout << "Введите число 1: ";
     std::cin >> x;
     std::cout << "Введите число 2: ";
     std::cin >> y;
-    std::cout << "Результат сложения: " << func(x, y) << std::endl;
+    std::cout << "Результат сложения: " << add(x, y) << std::endl;
 #else
     std::cout << "Неизвестный режим.Завершение работы" << std::endl;
 #endif
