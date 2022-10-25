f18cd57a797b2350c36dfebf4a06561302fac15b
Natalia Lyukshina <chip5171@gmail.com>
2022-10-25 14:32:24 +0900

    added homeworks_7

---
+++ homeworks_7/homework_7_1/homework_7_1.cpp
@@ -0,0 +1,31 @@
+﻿// homework_7_1.cpp : Задача 1. Препроцессорные директивы
+
+#include <iostream>
+#define MODE 1
+
+int func(int x, int y) {
+    return (x + y);
+}
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "rus");
+    int x, y;
+
+#ifndef MODE
+#error Сonstant is not defined
+#endif
+#if MODE == 0 
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите число 1: ";
+    std::cin >> x;
+    std::cout << "Введите число 2: ";
+    std::cin >> y;
+    std::cout << "Результат сложения: " << func(x, y) << std::endl;
+#else
+    std::cout << "Неизвестный режим.Завершение работы" << std::endl;
+#endif
+    return 0;
+}
+
