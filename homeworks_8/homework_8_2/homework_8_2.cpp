2656d6dd2000ac883b6797e5ba63ea7a0afb29b6
Natalia Lyukshina <chip5171@gmail.com>
2022-10-29 17:33:22 +0900

    modified homework_8_2

--- homeworks_8/homework_8_2/homework_8_2.cpp
+++ homeworks_8/homework_8_2/homework_8_2.cpp
@@ -15,28 +15,6 @@
 #include "rhombus.h"
 #include "square.h"
 
-//class Square : public Rectangle {
-//protected:
-//    std::string reason11 = "все стороны не равны между собой";
-//public:
-//    Square() {
-//        name = "Квадрат";
-//        a_ = b_ = c_ = d_ = 0;
-//        A_ = B_ = C_ = D_ = 90;
-//    }
-//    Square(int a, int b, int c, int d, int A, int B, int C, int D) {
-//        name = "Квадрат";
-//        a_ = a; b_ = b; c_ = c; d_ = d; A_ = A; B_ = B; C_ = C; D_ = D;
-//        std::string what7 = whatString(reason7);
-//        std::string what10 = whatString(reason10);
-//        std::string what11 = whatString(reason11);
-//        if (A_ + B_ + C_ + D_ != 360) throw FigureException(what7);
-//        if ((A_ != C_) && (B_ != D_)) throw FigureException(what10);
-//        if ((a_ != b_) && (b_ != c_) && (c_ != d_)) throw FigureException(what11);
-//    }
-//};
-
-
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "rus");
     Figure figure(0);
