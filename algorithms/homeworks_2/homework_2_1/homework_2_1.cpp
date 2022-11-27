03c613dbc62df06ceb2543986c8f56c38e28a89e
Natalia Lyukshina <chip5171@gmail.com>
2022-11-27 11:09:24 +0900

    added algorithms homework 2

---
+++ algorithms/homeworks_2/homework_2_1/homework_2_1.cpp
@@ -0,0 +1,27 @@
+﻿// homework_2_1.cpp : Рекурсивное нахождение чисел Фибоначчи на C++
+
+// Сложность алгоритма по скорости: O(2^n)
+
+// Сложность алгоритма по памяти: O(1)
+
+
+#include <iostream>
+
+unsigned long long int f(int n)
+{
+    return n < 2 ? n : f(n - 2) + f(n - 1);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    
+    unsigned int x;
+    std::cout << "Сколько вывести чисел?";
+    std::cin >> x;
+    std::cout << "Числа Фибоначчи:" << std::endl;
+    for (int i = 0; i < x; ++i) {
+        std::cout << i + 1 << ".\t" << "F(" << i << ") = " << f(i) << std::endl;
+    }
+    return 0;
+}
---
+++ algorithms/homeworks_2/homework_2_2/homework_2_2.cpp
@@ -0,0 +1,33 @@
+﻿// homework_2_2.cpp : Ускоряем Фибоначчи на C++
+
+// Сложность алгоритма по скорости: O(n)
+
+// Сложность алгоритма по памяти: O(n)
+
+
+#include <iostream>
+
+unsigned long long f(unsigned int n)
+{
+    unsigned long long first = 0, second = 1, result = n;
+    for (int i = 0; i < n; ++i) {
+        result = first + second;
+        first = second;
+        second = result;
+    }
+    return result;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    unsigned int x;
+    std::cout << "Сколько вывести чисел?";
+    std::cin >> x;
+    std::cout << "Числа Фибоначчи:" << std::endl;
+    for (int i = 0; i < x; ++i) {
+        std::cout << i + 1 << ".\t" << "F(" << i << ") = " << f(i) << std::endl;
+    }
+    return 0;
+}
