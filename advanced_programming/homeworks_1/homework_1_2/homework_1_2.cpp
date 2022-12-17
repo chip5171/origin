f3626b1490680843cf76da0d2cb31f9fafce6de4
Natalia Lyukshina <chip5171@gmail.com>
2022-12-18 02:20:22 +0900

    advansed_programming

---
+++ advanced_programming/homeworks_1/homework_1_2/homework_1_2.cpp
@@ -0,0 +1,50 @@
+﻿// homework_1_2.cpp : CPPL-5 Задача 2. std::variant
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <variant>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+int main(int argc, char** argv) {
+
+    setlocale(LC_ALL, "Rus");
+
+	std::variant<int, std::string, std::vector<int>> result = get_variant();
+		
+	if ((std::holds_alternative<std::string>(result)) == true) {
+		std::cout << "Строка: " << std::get<std::string>(result) << std::endl;
+	}
+	if ((std::holds_alternative<int>(result)) == true) {
+		std::cout << "Целое число: " << (std::get<int>(result)) * 2 << std::endl;
+	}
+	if ((std::holds_alternative<std::vector<int>>(result)) == true) {
+		std::vector<int> v = std::get<std::vector<int>>(result);
+		std::cout << "Вектор: ";
+		std::for_each(v.begin(), v.end(), [](const int v) { std::cout << v << ' '; });
+	}
+	
+	return 0;
+}
+
---
+++ advanced_programming/homeworks_1/homework_1_3/homework_1_3.cpp
@@ -0,0 +1,40 @@
+﻿// homework_1_2.cpp : CPPL-5 Задача 3*. Операции над углами
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <cmath>
+
+#define PI 3.14159265
+
+
+int main(int argc, char** argv) {
+
+	setlocale(LC_ALL, "Rus");
+	
+	std::vector<int> anglesDegrees = { 30, 60, 90 };
+	
+	std::vector<double> angles;
+	for (int i : anglesDegrees) {
+		angles.push_back(i * PI / 180);
+	}
+
+	auto f_cos = [](double a) {std::cout << "\b cos:" << cos(a); };
+	auto f_sin = [](double a) {std::cout << "\b sin:" << sin(a); };
+
+	std::vector<std::function<void(double)>> functions{f_sin, f_cos};
+	
+	std::cout << "[Входные данные]:";
+	std::for_each(anglesDegrees.begin(), anglesDegrees.end(), [](int ang) { std::cout << ang << " * 3.1415926 / 180,";});
+	std::cout << "\b // перевод из градусов в радианы" << std::endl;
+	
+	std::cout << "[Выходные данные]:" << std::endl;
+	for (const auto& angle : angles) {
+		std::cout << angle << ": ";
+		for (const auto& function : functions)
+			function(angle);
+		std::cout << std::endl;
+	}
+	return 0;
+}
