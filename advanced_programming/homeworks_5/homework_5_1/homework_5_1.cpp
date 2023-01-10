5386ace36a05bdcf0e1e36ce7de2f9f3ea5066f8
Natalia Lyukshina <chip5171@gmail.com>
2023-01-10 18:08:24 +0900

    added homeworks_5

---
+++ advanced_programming/homeworks_5/homework_5_1/homework_5_1.cpp
@@ -0,0 +1,39 @@
+﻿// homework_5_1.cpp : CPPL-5 Задача 1. Возведение в квадрат
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template <class T>
+T square(T n) {
+    return n * n;
+}
+
+template <class VT>
+std::vector<VT> square(std::vector<VT> v) {
+    for (int i = 0; i < v.size(); i++) {
+        v[i] = v[i] * v[i];
+    }
+    return v;
+}
+template <class VT>
+void printVector(VT v) {
+    std::for_each(v.begin(), v.end(), [](int v) { std::cout << v << ", "; });
+}
+
+int main(int argc, char** argv) {
+
+    int n = 4;
+    std::vector<int> v {-1, 4, 8 };
+
+    std::cout << "[IN]: " << n << std::endl;
+    std::cout << "[OUT]: " << square(n) << std::endl;
+
+    std::cout << "[IN]: "; 
+    printVector(v);
+    std::cout << std::endl;
+    std::cout << "[OUT]: ";
+    printVector(square(v));
+   
+    return 0;
+}
---
+++ advanced_programming/homeworks_5/homework_5_3/homework_5_3.cpp
@@ -0,0 +1,41 @@
+﻿// homework_5_3.cpp : CPPL-5 Задача 3. Счётчик чисел
+
+#include <algorithm>
+#include <iostream>
+
+class MyFunctor {
+private:
+    int sum_;
+    int count_;
+public:
+    MyFunctor() : count_(0), sum_(0) {}
+    void operator()(int x) {
+        if (x % 3 == 0) {
+            count_++;
+            sum_ += x;
+        }
+    }
+    int get_sum() const { return sum_; }
+    int get_count() const { return count_; }
+};
+
+int main(int argc, char** argv) {
+    
+    setlocale(LC_ALL, "Rus");
+    
+    MyFunctor mf;
+
+    int arr[] = { 4, 1, 3, 6, 25, 54 };
+    std::cout << "[IN]: ";
+    for (int value : arr) std::cout << value << ' ';
+    std::cout << std::endl;
+
+    mf = std::for_each(arr, arr + sizeof(arr) / sizeof(arr[0]), mf);
+
+    std::cout << "[OUT]: get_sum() = " << mf.get_sum() << "\n";
+    std::cout << "[OUT]: get_count() = " << mf.get_count() << "\n";
+
+    return 0;
+}
+
+
