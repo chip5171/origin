1ffdf6ac4ff3c65eac840d18722723050248feb7
Natalia Lyukshina <chip5171@gmail.com>
2023-01-21 18:00:33 +0900

    added homeworks_9

---
+++ advanced_programming/homeworks_9/homework_9_1/homework_9_1.cpp
@@ -0,0 +1,31 @@
+﻿// homework_9_1.cpp : CPPL-5 Задача 1. Перемещение объектов
+
+#include <iostream>
+#include <vector>
+
+template <class T>
+void move_vectors(std::vector<T>& a, std::vector<T>& b) {
+    b = std::move(a);
+}
+template <class T>
+void print_vector(std::vector<T>& v) {
+    for (const auto elem : v) { std::cout << elem << " "; }
+    std::cout << std::endl;
+}
+
+
+int main(int argc, char** argv) {
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+
+    move_vectors<std::string>(one, two);
+
+    std::cout << "one: ";
+    print_vector(one);
+  
+    std::cout << "two: ";
+    print_vector(two);
+          
+    return 0;
+}
+
---
+++ advanced_programming/homeworks_9/homework_9_2/homework_9_2.cpp
@@ -0,0 +1,127 @@
+﻿// homework_9_2.cpp : CPPL-5 Задача 2. Большие числа
+
+#include <iostream>
+#include <string>
+
+class big_integer {
+	std::string value; // значение числа
+
+public:
+	big_integer() {}
+	
+	big_integer(long x) : value(std::to_string(x)) {}
+
+	big_integer(const std::string& str) : value(str) {
+		if (!value.length()) {
+			this->value = "0";
+			return;
+		}
+		this->value = value.substr();
+
+		int count = 0;
+		while (this->value[count] == '0' && this->value.length() - count > 1)
+			count++;
+		this->value = this->value.substr(count); 
+
+		for (size_t i = 0; i < this->value.length(); i++)
+			if (this->value[i] < '0' || this->value[i] > '9')
+				throw std::exception("invalid character");
+	}
+	
+	big_integer(big_integer&& other) noexcept 
+		: value(std::exchange(other.value, nullptr)) {}
+	
+	big_integer& operator=(big_integer&& other) noexcept {
+		std::swap(value, other.value);
+		return *this;
+	}
+	
+	const std::string& getValue() const {
+		return value;
+	}
+
+	big_integer operator+(const big_integer& bigInt) const {
+		std::string num2 = bigInt.getValue(); 
+		size_t len1 = value.length(); 
+		size_t len2 = num2.length(); 
+		size_t length = 1 + std::max(len1, len2); 
+		char* res = new char[length + 1];
+		res[length - 1] = res[length] = '\0';
+
+		for (size_t i = 0; i < length - 1; i++) {
+			int j = length - 1 - i;
+			res[j] += ((i < len2) ? (num2[len2 - 1 - i] - '0') : 0) 
+				   + ((i < len1) ? (value[len1 - 1 - i] - '0') : 0); 
+			res[j - 1] = res[j] / 10; 
+			res[j] = res[j] % 10 + '0'; 
+		}
+		res[0] += '0';
+		
+		return big_integer(std::string(res));
+	}
+	
+	big_integer operator*(const big_integer& bigInt) const {
+		if (value == "0" || bigInt.getValue() == "0")
+			return 0; 
+		std::string value2 = bigInt.getValue(); 
+		size_t len1 = value.length(); 
+		size_t len2 = value2.length(); 
+		size_t length = len1 + len2 + 1; 
+		
+		if (length < 10) { 
+			long res = stol(value) * stol(value2);
+			return res;
+		}
+		else { 
+			int* a = new int[length];
+			int* b = new int[length];
+			char* res = new char[length + 1];
+			res[length] = '\0'; 
+
+			for (size_t i = 0; i < length; i++) {
+				a[i] = (i < len1) ? (value[len1 - 1 - i] - '0') : 0;
+				b[i] = (i < len2) ? (value2[len2 - 1 - i] - '0') : 0;
+				res[i] = 0;
+			}
+
+			for (size_t i = 0; i < len1; i++) {
+				for (size_t j = 0; j < len2; j++) {
+					res[length - 1 - (i + j)] += a[i] * b[j];
+					res[length - 1 - (i + j + 1)] += res[length - 1 - (i + j)] / 10;
+					res[length - 1 - (i + j)] %= 10;
+				}
+			}
+
+			for (size_t i = 0; i < length; i++)
+				res[length - 1 - i] += '0';
+
+			return big_integer(std::string(res));
+		}
+	}
+	
+	friend std::ostream& operator<<(std::ostream& stream, const big_integer& bigInt) {
+		return stream << bigInt.getValue();
+	}
+
+};
+
+int main(int argc, char** argv) {
+	
+	try {
+		auto number1 = big_integer("114575");
+		auto number2 = big_integer("78524");
+		auto result = number1 + number2;
+		std::cout << result << std::endl; 
+		
+		auto result1 = number1 * number2;
+		std::cout << result1 << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	
+	return 0;
+}
+
+
+
