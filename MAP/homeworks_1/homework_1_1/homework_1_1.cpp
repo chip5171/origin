a9e9e3d3221b4ac07cfc4b57e1fa0e3d270bb971
Natalia Lyukshina <chip5171@gmail.com>
2023-04-10 19:55:02 +0900

    added homeworks_1

---
+++ MAP/homeworks_1/homework_1_1/homework_1_1.cpp
@@ -0,0 +1,32 @@
+﻿// homework_1_1.cpp : MAP-5 Задание 1. «Очередь клиентов»
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono_literals;
+
+void client(int& count, int number) {
+    while (count < number) {
+        count++;
+        std::this_thread::sleep_for(1s);
+        std::cout << count << " clients in the queue. " << std::endl;
+    }
+}
+void clerk(int& count) {
+    while (count > 0) {
+        count--;
+        std::this_thread::sleep_for(2s);
+        std::cout << "The clerk processed the order, " << count << " clients in the queue. " << std::endl;
+    }
+}
+
+int main(int argc, char** argv) {
+    int c = 3;
+    std::thread t1(client, std::ref(c), 5);
+    std::thread t2 (clerk, std::ref(c));
+    t1.join();
+    t2.join();
+    
+    return 0;
+}
---
+++ MAP/homeworks_1/homework_1_2/homework_1_2.cpp
@@ -0,0 +1,58 @@
+﻿// homework_1_2.cpp : MAP-5 Задание 2 «Параллельные вычисления»
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <array>
+
+using namespace std::chrono_literals;
+
+std::vector<int> createVector(size_t size) {
+    std::vector<int> v(size);
+    for (int i = 0; i < size; i++) v[i] = i + 1;
+    return v;
+}
+
+std::vector<int> sumOfVectors(int size) {
+    std::vector<int> v1 = createVector(size);
+    std::vector<int> v2 = createVector(size);
+    std::vector<int> result;
+    std::transform(v1.begin(), v1.end(), v2.begin(), std::back_inserter(result), std::plus<int>());
+    return result;
+}
+
+//___________________________________________________________________________________
+    int main(int argc, char** argv) {
+        setlocale(LC_ALL, "rus");
+
+    std::array <int, 4> size{ 1000, 10000, 100000, 1000000 };
+    std::array <int, 5> thNumber{ 1, 2, 4, 8, 16 };
+
+    std::cout << "Количество аппаратных ядер: ";
+    std::cout << std::thread::hardware_concurrency() << std::endl;
+
+    for (const auto& i : size)
+        std::cout << "         " << i << "\t";
+    std::cout << std::endl;
+
+    for (const auto& j : thNumber) {
+        std::cout << j << " потоков ";
+        for (const auto& i : size) {
+            auto start = std::chrono::high_resolution_clock::now();
+            std::vector<std::thread> t;
+            for (int k = 0; k < j; k++) {
+                t.push_back(std::thread(sumOfVectors, i));
+            }
+            for (auto& t : t) { t.join(); }
+       
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration <double> time = end - start;
+            std::cout << time.count() << " \t";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
+
