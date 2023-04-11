ade388a2c989c4b83d3006d1e2646e8acc54f282
Natalia Lyukshina <chip5171@gmail.com>
2023-04-11 14:44:02 +0900

    modified homework_1_2

--- MAP/homeworks_1/homework_1_2/homework_1_2.cpp
+++ MAP/homeworks_1/homework_1_2/homework_1_2.cpp
@@ -15,44 +15,56 @@ std::vector<int> createVector(size_t size) {
     return v;
 }
 
-std::vector<int> sumOfVectors(int size) {
-    std::vector<int> v1 = createVector(size);
-    std::vector<int> v2 = createVector(size);
-    std::vector<int> result;
-    std::transform(v1.begin(), v1.end(), v2.begin(), std::back_inserter(result), std::plus<int>());
-    return result;
+void sumOfVectors(std::vector<int>& v1, std::vector<int>& v2, std::vector<int>& result,
+                  int beg, int end) {
+    for (auto i = beg; i < end; ++i) {
+        result[i] = v1[i] + v2[i];
+    }
+}
+
+void thSum(int thNumber, std::vector<int>& v1, std::vector<int>& v2, std::vector<int>& result, int size) {
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    std::vector<std::thread> t;
+    for (int k = 0; k < thNumber; k++) {
+    int beg = (size / thNumber) * k;
+    int end = (size / thNumber) * (k+1);
+        t.push_back(std::thread(sumOfVectors, std::ref(v1), std::ref(v2), std::ref(result), beg, end));
+    }
+    for (auto& t : t) { t.join(); }
+    
+    auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration <double> time = end - start;
+        std::cout << time.count() << " \t";
 }
 
 //___________________________________________________________________________________
+
     int main(int argc, char** argv) {
         setlocale(LC_ALL, "rus");
 
     std::array <int, 4> size{ 1000, 10000, 100000, 1000000 };
     std::array <int, 5> thNumber{ 1, 2, 4, 8, 16 };
-
+                
     std::cout << "Количество аппаратных ядер: ";
     std::cout << std::thread::hardware_concurrency() << std::endl;
 
-    for (const auto& i : size)
-        std::cout << "         " << i << "\t";
+    for (const auto& i : size) std::cout << "         " << i << "\t";
     std::cout << std::endl;
-
+    
     for (const auto& j : thNumber) {
         std::cout << j << " потоков ";
+        
         for (const auto& i : size) {
-            auto start = std::chrono::high_resolution_clock::now();
-            std::vector<std::thread> t;
-            for (int k = 0; k < j; k++) {
-                t.push_back(std::thread(sumOfVectors, i));
-            }
-            for (auto& t : t) { t.join(); }
-       
-            auto end = std::chrono::high_resolution_clock::now();
-            std::chrono::duration <double> time = end - start;
-            std::cout << time.count() << " \t";
+            std::vector<int> v1 = createVector(i);
+            std::vector<int> v2 = createVector(i);
+            std::vector<int> result(i);
+
+            thSum(j, v1, v2, result, i);
         }
         std::cout << std::endl;
     }
+        
     return 0;
 }
 
