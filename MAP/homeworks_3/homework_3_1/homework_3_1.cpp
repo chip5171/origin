39f87a59edf6cc940aaacbad2c115dd97fb941d7
Natalia Lyukshina <chip5171@gmail.com>
2023-05-16 20:41:49 +0900

    added homeworks_3

---
+++ MAP/homeworks_3/homework_3_1/homework_3_1.cpp
@@ -0,0 +1,60 @@
+﻿// homework_3_1.cpp: MAP-5 Задание 1 «Сортировка выбором»
+
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <future>
+#include <thread>
+
+int min(std::list<int> l) {
+    return *std::min_element(l.begin(), l.end());
+}
+
+void sortList(std::list<int>& l, std::promise<std::string>_prom) {
+    for (int j = 0; j < l.size(); j++) {
+        auto begin = std::next(l.begin(), j);
+        auto end = l.end();
+        std::list<int> sub;
+        sub.splice(sub.begin(), l, begin, end);
+
+        std::future<int> result(std::async(
+            std::launch::deferred, min, sub));
+        int m = result.get();
+
+        if (m != *sub.begin()) {
+            sub.insert(std::find(sub.begin(), sub.end(), m), *sub.begin());
+            sub.pop_front();
+            sub.remove(m);
+            sub.push_front(m);
+        }
+        l.splice(end, sub);
+    }
+    _prom.set_value("Sorting is complete");
+}
+
+void print(std::list<int>& l) {
+    for (const auto& elem : l)
+        std::cout << elem << " ";
+    std::cout << std::endl;
+}
+
+int main(int argc, char** argv) {
+
+    std::list<int> list = { 9, 6, 4, 3, 11, 2, 5, 7, 46 };
+    std::promise<std::string> prom;
+    std::future<std::string> fut = prom.get_future();
+
+    std::cout << std::endl;
+    print(list);
+
+    std::thread t(sortList, std::ref(list), std::move(prom));
+    fut.wait();
+    std::cout << fut.get() << std::endl;
+    print(list);
+   
+    t.join();
+    return 0;
+}
+
+
---
+++ MAP/homeworks_3/homework_3_2/homework_3_2.cpp
@@ -0,0 +1,52 @@
+﻿// homework_3_2.cpp: MAP-5 Задание 2 «for_each»
+
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <future>
+#include <thread>
+
+
+int func(int value) {
+	return value+1;
+}
+
+template<typename It, typename Func>
+void parallel_for_each(It first, It last, Func f) {
+	unsigned int const length = std::distance(first, last);
+	if (!length)
+		return;
+	unsigned int const min_per_thread = 25;
+	if (length < (2 * min_per_thread)) {
+		std::for_each(first, last, f); 
+	}
+	else {
+		It const mid_point = first + length / 2;
+		std::future<void> first_half = std::async(&parallel_for_each<It, Func>, first, mid_point, f);
+		std::future<void> last_half = std::async(&parallel_for_each<It, Func>,mid_point, last, f);
+		first_half.get(); 
+		last_half.get();
+	}
+}
+void print(std::vector<int>& l) {
+	for (const auto& elem : l)
+		std::cout << elem << " ";
+	std::cout << std::endl;
+}
+int main(int argc, char** argv) {
+	
+	std::vector<int> v{ 1,5,9,8,3,2 };
+	print(v);
+	int i = 0;
+	parallel_for_each(v.begin(), v.end(), [](int i) {std::cout << func(i) << " "; });
+	
+}
+
+
+
+
+
+
+
+
